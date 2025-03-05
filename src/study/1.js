8cbdb8c0f3d62131ec13654feb06202112cd9a0c
Zoe <sk-ssh-ed25519@openssh.com>
2025-03-05 14:18:17 +0800

    Feat: [init] #init

---
+++ src/study/1.js
@@ -0,0 +1,53 @@
+// 创建一个简单的双向绑定实现
+class Vue3Binding {
+    constructor(data) {
+      this.data = this.createProxy(data);
+    }
+  
+    // 创建代理对象
+    createProxy(data) {
+      const self = this;
+  
+      return new Proxy(data, {
+        get(target, prop) {
+          console.log(`Getting value of ${prop}:`, target[prop]);
+          return target[prop];
+        },
+        set(target, prop, value) {
+          console.log(`Setting value of ${prop}:`, value);
+          target[prop] = value;
+  
+          // 这里可以进行视图更新的逻辑，例如调用视图更新函数
+          self.updateView(prop, value);
+  
+          return true;
+        }
+      });
+    }
+  
+    // 模拟更新视图的函数
+    updateView(prop, value) {
+      console.log(`Update view for ${prop}:`, value);
+      // 这里可以进行实际的DOM更新操作
+    }
+  }
+  
+  // 使用实例
+  const data = {
+    message: 'Hello, Vue3!',
+  };
+  
+  // 创建 Vue3Binding 实例
+  const vueInstance = new Vue3Binding(data);
+  
+  // 模拟双向绑定：设置数据并观察如何更新
+  console.log(vueInstance.data.message); // 获取属性
+  vueInstance.data.message = 'Hello, Vue3 Proxy!'; // 设置属性，触发视图更新
+  
+  // 此时控制台将输出：
+  /*
+  Getting value of message: Hello, Vue3!
+  Setting value of message: Hello, Vue3 Proxy!
+  Update view for message: Hello, Vue3 Proxy!
+  */
+  
