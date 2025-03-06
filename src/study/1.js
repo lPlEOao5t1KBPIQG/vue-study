3f2acf1319e07364edaad11067ab89c85d4aca68
Zoe <sk-ssh-ed25519@openssh.com>
2025-03-06 10:59:03 +0800

    Feat: git add 2

--- src/study/1.js
+++ src/study/1.js
@@ -52,3 +52,15 @@ class Vue3Binding {
   */
   
   //fitst
+  // 冒泡排序
+  function bubbleSort(arr) {
+    const len = arr.length;
+    for (let i = 0; i < len; i++) {
+      for (let j = 0; j < len - 1 - i; j++) {
+        if (arr[j] > arr[j + 1]) {
+          [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+        }
+      }
+    }
+    return arr;
+  }
